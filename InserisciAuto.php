c893bd6f1d3c94f443a4e8e4295c5fbd21f255fc
AlessandroPeirone <38939422+AlessandroPeirone@users.noreply.github.com>
2019-11-20 12:54:55 +0100

    Add files via upload

---
+++ InserisciAuto.php
@@ -0,0 +1,31 @@
+<?php
+    
+    $cognome = $_REQUEST["cognome"];
+    $nome = $_REQUEST["nome"];
+    $email = $_REQUEST["email"];
+    $password = $_REQUEST["password"];
+
+    $marca = $_REQUEST["marca"];
+    $modello = $_REQUEST["modello"];
+
+    $optional = $_REQUEST["optional"];
+
+    $debug = 1;
+    if($debug)
+    {
+        echo "nome: " . $nome . "</br>";
+        echo "cognome: " . $cognome . "</br>";
+        echo "email: " . $email . "</br>";
+        echo "password: " . $password . "</br>";
+        echo "marca: " . $marca . "</br>";
+        echo "modello: " . $modello . "</br>";
+
+        echo "optional: ";
+		echo "<br>";
+		
+        foreach($optional as $opt)
+        {
+            echo $opt;
+        }
+    }
+?>
